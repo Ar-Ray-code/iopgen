924732647ba56497e9a2382def89549402c39176
Ar-Ray-code <ray255ar@gmail.com>
2023-01-25 12:03:41 +0900

    mv root

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ENTRY_TERMINAL_POINT=$(pwd)
 cd $SCRIPT_DIR/$PROJECT_NAME
 
-cargo build
+cargo build --release
 
 # failed
 if [ $? -ne 0 ]; then
@@ -14,10 +14,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "=============================="
-$SCRIPT_DIR/$PROJECT_NAME/target/debug/$PROJECT_NAME -c $SRC -o ./EXPORT.md -y example/ros2/ros2.yaml
+echo "============= IOP Gen ================="
+$SCRIPT_DIR/$PROJECT_NAME/target/release/$PROJECT_NAME -c $SRC -o ./EXPORT.md -y example/ros2/ros2.yaml
 
 echo ""
-echo "=============================="
+echo "======================================"
 
 cd -
